53f5ab1cd2447a3b145ac5261fb1ae55154126f8
Joannes <jbotelo@gmail.com>
2019-11-22 17:50:20 -0400

    clase 15 y 16

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace EmployeeManagement
 {
@@ -21,6 +21,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,10 +34,7 @@ namespace EmployeeManagement
 
             app.UseStaticFiles();
 
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync("Hosting Enviroment: "+env.EnvironmentName);
-            });
+            app.UseMvcWithDefaultRoute();
 
         }
     }
