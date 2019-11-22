a6f97367e972946cd9e9065c2f63292b2fd47c6a
Joannes <jbotelo@gmail.com>
2019-11-22 18:07:18 -0400

    clase 17

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public JsonResult Index()
         {
-            return "Hello desde metodo Index (((MVC)))";
+            return Json(new {id=1, name="Rex" });
         }
     }
 }
