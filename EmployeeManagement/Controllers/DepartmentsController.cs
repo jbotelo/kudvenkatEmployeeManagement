dc111ae6899491e1d9fab5ce639c34665d0a66c6
Joannes <jbotelo@gmail.com>
2019-11-24 12:26:26 -0400

    clase 32

---
+++ EmployeeManagement/Controllers/DepartmentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class DepartmentsController
+    {
+        public string List()
+        {
+            return "List() of Departaments";
+        }
+        public string Details()
+        {
+            return "Details() of Departments";
+        }
+    }
+}
--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -37,7 +37,11 @@ namespace EmployeeManagement
 
             app.UseStaticFiles();
 
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(routes=>{
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
 
         }
     }
