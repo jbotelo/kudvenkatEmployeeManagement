5967ee1dd4bd811395ddbba3ff490bd23518bb8b
Joannes <jbotelo@gmail.com>
2019-11-29 16:39:09 -0400

    clase 87(parte2)

--- EmployeeManagement/wwwroot/js/CustomScript.js
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -1,1 +1,12 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    var delelteSpan = "deleteSpan_" + uniqueId
+    var confirmDeleteSpan = "confirmDeleteSpan_" + uniqueId
+
+    if (isDeleteClicked) {
+        $('#' + delelteSpan).hide()
+        $('#' + confirmDeleteSpan).show()
+    } else {
+        $('#' + delelteSpan).show()
+        $('#' + confirmDeleteSpan).hide()
+    }
+}
