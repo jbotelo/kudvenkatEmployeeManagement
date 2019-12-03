0b1b4411a50f981da735b28fbbf8c2949fa155d4
Joannes <jbotelo@gmail.com>
2019-12-03 10:49:15 -0400

    clase 122

---
+++ EmployeeManagement/ViewModels/AddPasswordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class AddPasswordViewModel
+    {
+        [Required, DataType(DataType.Password), Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name = "New Password")]
+        [Compare("NewPassword", ErrorMessage =
+                  "The new password and confirmation password do not match.")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
